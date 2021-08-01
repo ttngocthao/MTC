23aad0533a07c192c9aa61aea876df80c1a4063d
ttngocthao <ttngocthao_87@yahoo.com>
2021-08-01 06:11:32 +0100

    updated footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,23 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
-import FaceBookIcon from '../../images/facebook_footer.svg'
-import InstagramIcon from '../../images/instagram_footer.svg'
-import LinkedInIcon from '../../images/linkedIn_footer.svg'
+import FaceBookIcon from '../../images/footer/facebook.svg'
+import InstagramIcon from '../../images/footer/instagram.svg'
+import LinkedInIcon from '../../images/footer/linkedin.svg'
+import TrustpilotIcon from '../../images/footer/trustpilot.svg'
+import DribbleIcon from '../../images/footer/dribble.svg'
+import BehanceIcon from '../../images/footer/behance.svg'
+import TwitterIcon from '../../images/footer/twitter.svg'
+
+
 const socialMediaData = [
     {
         iconImg: LinkedInIcon,
-        linkUrl: '#',
-        orderInList: 1
+        linkUrl: 'https://www.linkedin.com/company/mighty-cultured/',
+        orderInList: 2
     },
     {
         iconImg: InstagramIcon,
         linkUrl: 'https://www.instagram.com/mightyculturedltd/',
-        orderInList: 2
+        orderInList: 5
     },
     {
         iconImg: FaceBookIcon,
-        linkUrl: 'https://www.facebook.com/Mighty-Cultured-426372721081128',
-        orderInList: 3
+        linkUrl: 'https://www.facebook.com/mightycultured',
+        orderInList: 7
+    },
+    {
+        iconImg:TrustpilotIcon,
+        linkUrl:'https://uk.trustpilot.com/review/www.mightycultured.co.uk',
+        orderInList:1
+    },
+    {
+        iconImg:DribbleIcon,
+        linkUrl:'https://dribbble.com/MightyCultured',
+        orderInList:3
+    },
+    {
+        iconImg:BehanceIcon,
+        linkUrl:'https://www.behance.net/mightycultured',
+        orderInList:4
+    },
+    {
+        iconImg:TwitterIcon,
+        linkUrl:'https://twitter.com/MightyCultured',
+        orderInList:6
     }
 ]
 
@@ -30,10 +56,13 @@ const StyledFooter = styled.div`
     /* max-width:1640px; */
     margin:0 auto;
     color: #222;
-    text-align: right; 
-    padding:2rem 1rem;
+    display: block;
+    padding:2rem;
+    position: relative;
     @media only screen and (min-width: 700px){
-       
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
          padding:2rem 7.3%;
        
        
@@ -45,15 +74,39 @@ const SocialMediaItem = styled.a`
     display: inline-block;
     margin-left:.5rem;
     box-sizing: border-box;
+    &:first-child{
+        margin-left: 0;
+    }
+`
+const CopyRight = styled.div`
+    /* position: absolute;
+    top:2rem;
+    left:2rem; */
+    padding:0rem 0 1rem;
+     @media only screen and (min-width: 700px){         
+        /* left: 7.3%; */
+       
+       
+         
+     }
 `
 const Footer = () => {
-
+    const orderedList = socialMediaData.sort((a,b)=>{
+        if(a.orderInList>b.orderInList) return 1
+        if(a.orderInList<b.orderInList) return -1
+        return 0
+    })
+    const currentYear = new Date().getFullYear();
+  
+    console.log('currentYear',currentYear)
     return (
         <Wrap>
             <StyledFooter>
-            {socialMediaData.map((item, index) => <SocialMediaItem key={index} target='_blank' href={item.linkUrl}>
+            <CopyRight>© Mighty Cultured Ltd {currentYear}</CopyRight>
+            <div>
+            {orderedList.map((item, index) => <SocialMediaItem key={index} target='_blank' href={item.linkUrl}>
                 <img src={item.iconImg} style={{ width: '1.5rem' }} alt='' />
-            </SocialMediaItem>)}
+            </SocialMediaItem>)}</div>
         </StyledFooter>
         </Wrap>
         
