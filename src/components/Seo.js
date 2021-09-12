e40ef9124a676adebf66e2bba19bac1433ee8ee0
ttngocthao <ttngocthao_87@yahoo.com>
2021-09-12 21:16:21 +0100

    updated description

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -11,7 +11,7 @@ const Seo = ({title}) => {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;600;900&display=swap" rel="stylesheet" />
              <html lang="en" amp />
-            <meta name="description" content="Mighty cultured - branding, designing, digital" />
+            <meta name="description" content="Mighty cultured is a branding and creative studio working with businesses that enjoy the benefits of change, that out-think the competition and see the potential in strong brand presence." />
     
         </Helmet>
     )
